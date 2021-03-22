7fd7f17b6db93be6f8f1372c64371dbd144348e0
Pratham443 <77573159+Pratham443@users.noreply.github.com>
2021-03-22 18:27:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+
+var mouseEvent = "none";
+var last_X, last_Y;
+var color = "red";
+var lineWidth = 2;
+var radius = 20;
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    lineWidth = document.getElementById("line-width").value;
+    radius = document.getElementById("radius").value;
+
+    mouseEvent = "mouseDown";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e) {
+    mouseEvent = "mouseup";
+}
+
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e) {
+    mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e) {
+    current_X = e.clientX - canvas.offsetLeft;
+    current_Y = e.clientY - canvas.offsetTop;
+
+    if(mouseEvent == "mouseDown") {
+        console.log("current X and Y are:");
+        console.log("X: " + current_X + "Y: " + current_Y);
+
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.arc(current_X, current_Y, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+}
+
+function clear() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
